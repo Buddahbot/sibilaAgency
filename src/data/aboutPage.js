6799780e4763d981dc5a6dc66fd7b832a43c5655
Buddahbot <90890525+Buddahbot@users.noreply.github.com>
2024-06-18 10:50:55 +0200

    changed image of me and text in about

--- src/data/aboutPage.js
+++ src/data/aboutPage.js
@@ -1,15 +1,15 @@
 import badge from "@/images/resources/about-page-badge.png";
 
 const aboutPage = {
-  images: ["womenatdesk2resizedcropped2.jpg"],
+  images: ["sebastian4re.jpeg"],
   badge,
-  tagline: "about us",
-  title: "Transforming Ideas into Digital Reality",
-  text1: "Welcome to Sibila Digital Agency, where we are passionate about crafting innovative digital solutions tailored to your needs. Specializing in web app development, we harness cutting-edge technologies including Node.js, React, React Native, Solidity, MongoDB, MySQL, Strapi, PostgreSQL, and more to bring your ideas to life.",
-  text2: "With a keen focus on delivering high-quality results, we strive to exceed your expectations every step of the way. Whether you're a startup looking to establish your online presence or an established business aiming to enhance your digital footprint, we're here to help.",
-  text3: "Our team of skilled developers, designers, and strategists collaborates closely with you to understand your vision and objectives, ensuring that every project we undertake is aligned with your goals.",
-  text4: "From concept to deployment, we prioritize transparency, communication, and responsiveness, ensuring a smooth and seamless development process. Your satisfaction is our ultimate priority, and we are committed to delivering solutions that drive success for your business.",
-  text5: "Let's embark on a journey to transform your ideas into reality and propel your business forward.",
+  tagline: "about me",
+  title: "Hi There",
+  text1: " I'm Sebastian, a passionate programmer with a unique journey. I started my career in the vibrant music scenes of Berlin and Australia, playing in a hardcore crossover band. Those years were filled with creativity and collaboration.",
+  text2: "Eventually, I discovered my passion in programming, so I immersed myself in the world of code. The thrill of solving complex problems, creating innovative solutions, and continuously learning new technologies captivated me.",
+  text3: "Today, I channel my creative energy and dedication into developing sleek, efficient applications and tackling intricate coding challenges. In addition to my technical work, I also lead teams, guiding projects from concept to completion.",
+  text4: "Let's connect and create something amazing together!",
+  text5: "",
 };
 
 export default aboutPage;
